c00cb01144b0b3da261cdb6fd647472b7684f73a
ramajn <110238183+ramajn@users.noreply.github.com>
2024-08-28 08:54:09 -0300

    Add files via upload

---
+++ siteminoriasediego-main/script/script.js
@@ -0,0 +1,120 @@
+let entrevista_1 = {
+    entrevistador:"Rama",
+    entrevistado:"Vitor",
+}
+questoes:{
+    p1:"Qual é a sua maior paixão?";
+    r1:"Biologia";
+    p2:"Qual é o seu maior sonho?";
+    r2:"Ser rico";
+    p3:"Qual é o seu maior medo?";
+    r3:"Ficar sozinho";
+    p4:"Quais são seus hobbies?";
+    r4:"Ler e imaginar cenarios fecticios ";
+    p5:"O que você quer ser quando crescer?";
+    r5:"Uma pessoa com sucesso no que faz";
+    p6:"O que você queria ser quando era pequeno?";
+    r6:"Youtuber";
+    p7:"Qual é o seu filme ou livro preferido?";
+    r7:"Minha mãe é uma peça e Aristóteles e Dante";
+    p8:"Como você acha que seus pais descreveriam você?";
+    r8:"Soberbo, inteligente, engraçado, fala demais, ";
+    p9:"Qual o acontecimento mais marcante da sua vida?";
+    r9:"O dia que assumi minha sexualidade ";
+    p10:"Quais são as suas maiores qualidades e defeitos?";
+    r10:"Arrogância e ignorância, as qualidades inteligência e companheirismo";
+    p11:"Você mudaria algo em você? o que seria?";
+    r11:"Fisicamente gostaria de ser mais alto";
+    p12:"Caso tenha só mais um dia de vida, o que você faria?";
+    r12:"Cometer alguns crimes";
+}
+let entrevista_2 = {
+    entrevistador:"Vitor",
+    entrevistado:"Laura",
+}
+questoes:{
+    p1:"Qual é a sua maior paixão?";
+    r1:"";
+    p2:"Qual é o seu maior sonho?";
+    r2:"";
+    p3:"Qual é o seu maior medo?";
+    r3:"";
+    p4:"Quais são seus hobbies?";
+    r4:"";
+    p5:"O que você quer ser quando crescer?";
+    r5:"";
+    p6:"O que você queria ser quando era pequeno?";
+    r6:"";
+    p7:"Qual é o seu filme ou livro preferido?";
+    r7:"";
+    p8:"Como você acha que seus pais descreveriam você?";
+    r8:"";
+    p9:"Qual o acontecimento mais marcante da sua vida?";
+    r9:"";
+    p10:"Quais são as suas maiores qualidades e defeitos?";
+    r10:"";
+    p11:"Você mudaria algo em você? o que seria?";
+    r11:"";
+    p12:"Caso tenha só mais um dia de vida, o que você faria?";
+    r12:"";
+}
+let entrevista_3 = {
+    entrevistador:"Laura",
+    entrevistado:"Diego",
+}
+questoes:{
+    p1:"Qual é a sua maior paixão?";
+    r1:"";
+    p2:"Qual é o seu maior sonho?";
+    r2:"";
+    p3:"Qual é o seu maior medo?";
+    r3:"";
+    p4:"Quais são seus hobbies?";
+    r4:"";
+    p5:"O que você quer ser quando crescer?";
+    r5:"";
+    p6:"O que você queria ser quando era pequeno?";
+    r6:"";
+    p7:"Qual é o seu filme ou livro preferido?";
+    r7:"";
+    p8:"Como você acha que seus pais descreveriam você?";
+    r8:"";
+    p9:"Qual o acontecimento mais marcante da sua vida?";
+    r9:"";
+    p10:"Quais são as suas maiores qualidades e defeitos?";
+    r10:"";
+    p11:"Você mudaria algo em você? o que seria?";
+    r11:"";
+    p12:"Caso tenha só mais um dia de vida, o que você faria?";
+    r12:"";
+}
+let entrevista_4 = {
+    entrevistador:"Diego",
+    entrevistado:"Rama",
+}
+questoes:{
+    p1:"Qual é a sua maior paixão?";
+    r1:"";
+    p2:"Qual é o seu maior sonho?";
+    r2:"";
+    p3:"Qual é o seu maior medo?";
+    r3:"";
+    p4:"Quais são seus hobbies?";
+    r4:"";
+    p5:"O que você quer ser quando crescer?";
+    r5:"";
+    p6:"O que você queria ser quando era pequeno?";
+    r6:"";
+    p7:"Qual é o seu filme ou livro preferido?";
+    r7:"";
+    p8:"Como você acha que seus pais descreveriam você?";
+    r8:"";
+    p9:"Qual o acontecimento mais marcante da sua vida?";
+    r9:"";
+    p10:"Quais são as suas maiores qualidades e defeitos?";
+    r10:"";
+    p11:"Você mudaria algo em você? o que seria?";
+    r11:"";
+    p12:"Caso tenha só mais um dia de vida, o que você faria?";
+    r12:"";
+}
